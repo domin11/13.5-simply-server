b18477b1b97e3beea2411d3f006ec44b27bb948e
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-03 16:42:15 +0200

    Modify index.js

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ var http = require('http');
 var server = http.createServer();
 
 server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/') {
-		fs.readFile('./index.html', 'utf-8', function(err, data) {
-			response.write(data);
-            response.end();
-		});	
-    } else {
-		 fs.readFile('../images/404.jpg', function(err, data) {
+    if (request.method === 'GET') {
+		if (request.url === '/') {
+			response.setHeader("Content-Type", "text/html; charset=utf-8");
+			fs.readFile('./index.html', 'utf-8', function(err, data) {
+				response.write(data);
+				response.end();
+			});
+		} else if (request.url === '/css/style.css') {
+			response.setHeader("Content-Type", "text/css");
+			fs.readFile('./css/style.css', function(err, data) {
+				response.write(data);
+				response.end();
+			});	
+		}	
+	} else {
+		response.setHeader("Content-Type", "image/jpeg");
+		fs.readFile('./images/404.jpg', function(err, data) {
 			if (err) throw err;
             response.statusCode = 404;
             response.write(data);
